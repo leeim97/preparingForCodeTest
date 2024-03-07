03fe3f16e90d6827a31763447cb196157ad0240f
Imhyung Lee <rhdtn987@gmail.com>
2024-03-08 01:52:27 +0900

    suroso set algorithm and zip the root

---
+++ 나동빈 이코테/ch.10 그래프 이론/10_2_경로 압축 기법이 적용된 서로소 집합 알고리즘.py
@@ -0,0 +1,37 @@
+def find_parent(parent,x):
+    if parent[x] !=x:
+        parent[x] = find_parent(parent,parent[x])
+
+    return parent[x]
+
+def union(parent,a,b):
+    if find_parent(parent,a) != find_parent(parent,b):
+        parent_a= find_parent(parent,a)
+        parent_b=find_parent(parent,b)
+        if parent_a > parent_b:
+            parent[parent_a] = parent_b
+        elif parent_a < parent_b:
+            parent[parent_b] = parent_a
+    else:
+        print('사이클 발생')
+        return
+
+v,e = map(int,input().split())
+parent=[0 for _ in range(v+1)]
+
+print(parent)
+for i in range(1,v+1):
+    parent[i]=i
+
+for _ in range(e):
+    a,b=map(int,input().split())
+    union(parent,a,b)
+
+print('각 원소가 속한 집합 : ')
+for i in range(1,v+1):
+    print(find_parent(parent,i),end=' ')
+print()
+
+print('부모 테이블')
+for i in range(1,v+1):
+    print(parent[i],end=' ')
