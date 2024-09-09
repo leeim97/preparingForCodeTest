7ab7819c67994da58ef8db4c8cd48271c9bc867a
Imhyung Lee <rhdtn987@gmail.com>
2024-09-09 19:31:29 +0900

    solve DFS BFS

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,45 @@
 from collections import deque
 
+
 # BFS
+# def solution(numbers, target):
+#     answer = 0
+#     leaves = deque([0])
+
+#     for i in numbers:
+#         temp = []
+#         while leaves:
+#             leaf = leaves.popleft()
+#             temp.append(leaf+i)
+#             temp.append(leaf-i)
+#         leaves = deque(temp)
+
+#     for leaf in leaves:
+#         if leaf == target:
+#             answer +=1
+
+#     return answer
+
+# DFS
 def solution(numbers, target):
     answer = 0
-    leaves = deque([0])
-    
-    for i in numbers:
-        temp = []
-        while leaves:
-            leaf = leaves.popleft()
-            temp.append(leaf+i)
-            temp.append(leaf-i)
-        leaves = deque(temp)
-        
-    for leaf in leaves:
-        if leaf == target:
-            answer +=1
-            
-    return answer                                                                                                                                                                                                                                                        
+    answer = DFS(numbers, target, 0)
+
+    return answer
+
+
+def DFS(numbers, target, depth):
+    answer = 0
+    if depth == len(numbers):
+        if sum(numbers) == target:
+            answer += 1
+            return 1
+        else:
+            return 0
+
+    else:
+        answer += DFS(numbers, target, depth + 1)
+        numbers[depth] *= -1
+        answer += DFS(numbers, target, depth + 1)
+        return answer
+
