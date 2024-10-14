1287611c963ea59cd44d9e7224794e62179b3e1c
Imhyung <107461780+leeim97@users.noreply.github.com>
2024-10-14 11:18:20 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,16 +1,5 @@
-def solution(brown, yellow):
-    # n은 가로 m은 세로
-    answer = []
-    
-    for i in range(1,yellow+brown):
-        if yellow % i ==0:
-            n=i
-            m= yellow/i
-        
-            if 2*(n+m+2) == brown:
-                answer.append(m+2)
-                answer.append(n+2)
-                
-                break
-    
-    return answer
+def solution(brown, red):
+    for i in range(1, int(red**(1/2))+1):
+        if red % i == 0:
+            if 2*(i + red//i) == brown-4:
+                return [red//i+2, i+2]
