53553cdf95339d17d43bbc304c21b67f99f4a190
Imhyung Lee <rhdtn987@gmail.com>
2024-11-16 00:13:45 +0900

    first

--- sol.py
+++ sol.py
@@ -1,15 +1,55 @@
+# dict의 get메소드 
+# dic.get(key,value)
+# key에 해당하는거 찾아줌 없으면 value 반환 value 안쓰면 None 반환
+
+import sys
+
 num = int(input())
-arr = list(map(int,input().split()))
-print(num,arr)
 
-answer = 0
-visit = [False]*9
+arr1 = list(map(int,sys.stdin.readline().split()))
 
-def bfs(num, arr, cnt):
-    global answer,visit
-    answer = max(answer,cnt)
+num2 = int(input())
 
-    for i,idx in enumerate(arr):
-        visit[i] = True
-        bfs(num,arr,cnt)
-        visit[i] = False
+arr2 = list(map(int,sys.stdin.readline().split()))
+
+arr1.sort()
+
+dic = {}
+
+# 틀린 답안
+# for i in set(arr1):
+#     dic[i] = arr1.count(i)
+
+# 이 방법 생각을 못했네 O(n)을써서 개수 구하는거
+for i in arr1:
+    if i in dic:
+        dic[i]+=1
+    else:
+        dic[i]=1
+
+
+# 찾기
+# 이것도됌
+# for j in arr2:
+#     print(dic.get(j,0),end = ' ')
+
+
+for j in arr2:
+    start = 0
+    end = len(arr1)-1
+
+    while start <= end:
+        mid = (start + end)//2
+
+        if arr1[mid] == j:
+            print(dic[j])
+            break
+
+        elif arr1[mid] > j:
+            end = mid -1
+
+        elif arr1[mid] < j:
+            start = mid +1
+
+    if start > end:
+        print(0)
--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -16,12 +16,40 @@ arr1.sort()
 
 dic = {}
 
+# 틀린 답안
+# for i in set(arr1):
+#     dic[i] = arr1.count(i)
+
+# 이 방법 생각을 못했네 O(n)을써서 개수 구하는거
 for i in arr1:
     if i in dic:
         dic[i]+=1
     else:
         dic[i]=1
 
-# print(dic)
+
+# 찾기
+# 이것도됌
+# for j in arr2:
+#     print(dic.get(j,0),end = ' ')
+
+
 for j in arr2:
-    print(dic.get(j,0),end = ' ')
+    start = 0
+    end = len(arr1)-1
+
+    while start <= end:
+        mid = (start + end)//2
+
+        if arr1[mid] == j:
+            print(dic[j])
+            break
+
+        elif arr1[mid] > j:
+            end = mid -1
+
+        elif arr1[mid] < j:
+            start = mid +1
+
+    if start > end:
+        print(0)
