8fe4c2a5cccb5523651ffbbc5e208b2663612f4c
Imhyung <107461780+leeim97@users.noreply.github.com>
2024-12-27 14:57:47 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+# 스킬 코드에서 언어들 확인 후 PYTHON, C#에 해당하는 사람
+SELECT 
+    ID,EMAIL,FIRST_NAME, LAST_NAME
+FROM 
+    DEVELOPERS A 
+WHERE
+    A.SKILL_CODE & (SELECT CODE FROM SKILLCODES 
+                   WHERE NAME = 'PYTHON')
+    OR  A.SKILL_CODE & (SELECT CODE FROM SKILLCODES 
+                   WHERE NAME = 'C#')
+ORDER BY ID;
