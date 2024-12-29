93ea96ebbe6a80f301b41c2ae471819fced87183
Imhyung <107461780+leeim97@users.noreply.github.com>
2024-12-29 21:37:13 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,16 +1,39 @@
 -- 코드를 작성해주세요
-SELECT 
-    ID, FISH_NAME, LENGTH
-FROM
-    FISH_INFO A
-JOIN
-    FISH_NAME_INFO B
-ON
-    A.FISH_TYPE = B.FISH_TYPE
-WHERE (A.FISH_TYPE, A.LENGTH) IN (
-    SELECT 
-        FISH_TYPE, MAX(LENGTH)
-    FROM 
-        FISH_INFO
-    GROUP BY FISH_TYPE
-        );
+# 제일 큰놈의 ID와 길이를 알아오자
+SELECT A.ID, B.FISH_NAME, A.LENGTH
+FROM FISH_INFO A
+JOIN FISH_NAME_INFO B
+ON A.FISH_TYPE = B.FISH_TYPE
+WHERE 
+    (A.FISH_TYPE, LENGTH) IN 
+                        (SELECT FISH_TYPE, MAX(LENGTH)
+                            FROM FISH_INFO
+                            GROUP BY FISH_TYPE
+                        )
+    
+# GROUP BY B.FISH_TYPE
+# HAVING A.LENGTH = MAX(A.LENGTH)
+# ORDER BY ID ASC;
+
+
+
+
+
+
+
+
+# SELECT 
+#     ID, FISH_NAME, LENGTH
+# FROM
+#     FISH_INFO A
+# JOIN
+#     FISH_NAME_INFO B
+# ON
+#     A.FISH_TYPE = B.FISH_TYPE
+# WHERE (A.FISH_TYPE, A.LENGTH) IN (
+#     SELECT 
+#         FISH_TYPE, MAX(LENGTH)
+#     FROM 
+#         FISH_INFO
+#     GROUP BY FISH_TYPE
+#         );
