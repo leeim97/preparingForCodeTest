2e532e3688f36c0bb71c521730f8c9e134c4ff59
Imhyung <107461780+leeim97@users.noreply.github.com>
2024-12-29 19:27:24 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,19 +1,43 @@
 -- 코드를 작성해주세요
-SELECT 
-    E.ID, COALESCE(B.CNT,0) AS CHILD_COUNT
+# SELECT ID,
+# FROM ECOLI_DATA A
+# JOIN ECOLI_DATA B
+# ON A.ID = B.PARENT_ID
+
+SELECT ID, COALESCE(B.CNT,0) AS CHILD_COUNT
 FROM
-    ECOLI_DATA E
-    LEFT JOIN 
-    (   SELECT
-            PARENT_ID, COUNT(*) AS CNT 
-         FROM 
-            ECOLI_DATA
-         GROUP BY
-            PARENT_ID
-         HAVING CNT IS NOT NULL
-    ) AS B
-ON 
-    B.PARENT_ID = E.ID
-ORDER BY ID ASC;
+    ECOLI_DATA A
+LEFT JOIN (SELECT PARENT_ID, COUNT(*) AS CNT
+     FROM ECOLI_DATA
+     GROUP BY PARENT_ID
+     HAVING PARENT_ID IS NOT NULL) B
+ON A.ID = B.PARENT_ID
+ORDER BY A.ID
+
+
+
+
+
+
+
+
+
+
+# SELECT 
+#     E.ID, COALESCE(B.CNT,0) AS CHILD_COUNT
+# FROM
+#     ECOLI_DATA E
+#     LEFT JOIN 
+#     (   SELECT
+#             PARENT_ID, COUNT(*) AS CNT 
+#          FROM 
+#             ECOLI_DATA
+#          GROUP BY
+#             PARENT_ID
+#          HAVING CNT IS NOT NULL
+#     ) AS B
+# ON 
+#     B.PARENT_ID = E.ID
+# ORDER BY ID ASC;
 
 # SELECT * FROM ECOLI_DATA;
