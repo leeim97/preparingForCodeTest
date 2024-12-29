8623ac52c15cc48661d7620f27a330fee0e62cf7
Imhyung <107461780+leeim97@users.noreply.github.com>
2024-12-29 21:48:40 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,12 +1,45 @@
 -- 코드를 작성해주세요
 # 스킬 코드에서 언어들 확인 후 PYTHON, C#에 해당하는 사람
-SELECT 
-    ID,EMAIL,FIRST_NAME, LAST_NAME
-FROM 
-    DEVELOPERS A 
-WHERE
-    A.SKILL_CODE & (SELECT CODE FROM SKILLCODES 
-                   WHERE NAME = 'PYTHON')
-    OR  A.SKILL_CODE & (SELECT CODE FROM SKILLCODES 
-                   WHERE NAME = 'C#')
-ORDER BY ID;
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE 
+    SKILL_CODE & (SELECT CODE
+                  FROM SKILLCODES
+                  WHERE 
+                    NAME ='C#')
+                  OR 
+                      SKILL_CODE & (SELECT CODE
+                  FROM SKILLCODES
+                  WHERE 
+                    NAME ='PYTHON')
+ORDER BY ID
+
+
+
+
+
+
+
+
+
+# SELECT 
+#     ID,EMAIL,FIRST_NAME, LAST_NAME
+# FROM 
+#     DEVELOPERS A 
+# WHERE
+#     A.SKILL_CODE & (SELECT CODE FROM SKILLCODES 
+#                    WHERE NAME = 'PYTHON')
+#     OR  A.SKILL_CODE & (SELECT CODE FROM SKILLCODES 
+#                    WHERE NAME = 'C#')
+# ORDER BY ID;
+
+
+# SELECT 
+#     ID,EMAIL,FIRST_NAME, LAST_NAME
+# FROM 
+#     DEVELOPERS A 
+# WHERE
+#     A.SKILL_CODE & (
+#     SELECT CODE FROM SKILLCODES
+#     WHERE NAME IN ('Python','C#'))
+# ORDER BY ID;
